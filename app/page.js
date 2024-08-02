2bbe07bbc68ca1bb8a7246f2b5eb38b88577b3aa
gyurisc <gyurisc@gmail.com>
2024-08-02 09:49:23 +0200

    adding icons

--- app/page.js
+++ app/page.js
@@ -1,12 +1,32 @@
 import Image from "next/image";
 import Writer from "../components/Writer";
+import Link from "next/link";
+import { GlobeIcon } from "../components/Icons";
+import { TwitterIcon } from "../components/Icons";
+import { GithubIcon } from "../components/Icons";
 
 export default function Home() {
   return (
+    <>
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <div className="z-10 max-w-5xl w-full flex flex-col items-center justify-between font-mono text-sm h-full lg:flex-row">
-        <Writer />
+        <div className="flex flex-col h-full w-full">          
+          <Writer />
+          <footer className="bg-muted px-2 py-2 text-xs text-muted-foreground flex justify-center">
+            Links:&nbsp;
+            <Link href="https://x.com/gyurisc" className="ml-2" prefetch={false}>
+              <TwitterIcon className="h-4 w-4" />
+            </Link>
+            <Link href="https://gyurisc.com" className="ml-2" prefetch={false}>
+              <GlobeIcon className="h-4 w-4" />
+            </Link>
+            <Link href="https://github.com/gyurisc/" className="ml-2" prefetch={false}>
+              <GithubIcon className="h-4 w-4" />
+            </Link>
+          </footer>          
+        </div>
       </div>
     </main>
+    </>
   );
 }
--- components/Writer.jsx
+++ components/Writer.jsx
@@ -28,6 +28,9 @@ To read more about using these font, please visit the Next.js documentation:
 
 import React, { useState } from 'react'
 import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
+import { MailIcon, GithubIcon, GlobeIcon, TwitterIcon } from "@/components/icons"
+
 
 
 const Writer = () => {
@@ -43,21 +46,25 @@ const Writer = () => {
   };
 
  return (
-    <div className="flex flex-col h-full w-full">
+    <>
       <div className="flex-1 pb-6 flex flex-col">
         <Textarea
           placeholder="Good Morning! Start Writing..."
           value={text}
           onChange={handleChange}
           className="w-full flex-1 resize-none p-4 rounded-lg border border-input focus:border-primary focus:ring-1 focus:ring-primary"
-        />
+          />
       </div>
       <div className="bg-muted px-6 py-4 flex justify-between items-center border-t">
         <div className="text-sm text-muted-foreground">
           <span id="word-count">{wordCount}</span> words, <span id="char-count">{charCount}</span> characters
         </div>
+        {/* <Button variant="ghost" size="icon" className="ml-auto">
+          <MailIcon className="h-5 w-5" />
+          <span className="sr-only">Share to email</span>
+        </Button> */}
       </div>
-    </div>
+    </>
   );
 }
 
